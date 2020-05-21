03495d0061b0bf3cc66468f6b8013afdb9662d6a
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-21 13:45:04 +1000

    Added fs

--- Person.js
+++ Person.js
@@ -1,4 +1,7 @@
+const fs = require("fs");
+const path = require("path");
 const uuid = require("uuid");
+
 const getRandomGivenName = function () {
     const RANDOM_FIRST_NAMES = ["Charlotte", "Olivia", "Ava", "Amelia", "Mia", "Isla", "Oliver", "William", "Jack", "Noah", "Thomas", "James"];
     return RANDOM_FIRST_NAMES[Math.floor(Math.random() * RANDOM_FIRST_NAMES.length)];
@@ -62,7 +65,7 @@ class Student extends Person {
         ));
     }
 
-    static generateRandomGrades () {
+    static generateRandomGrades() {
         let numberOfGrades = getRandomNumber(10);
         let grades = [];
         for (let i = 0; i < numberOfGrades; i++) {
@@ -103,10 +106,20 @@ class Teacher extends Person {
     }
 }
 
-let teachers = Teacher.generateRandomPeople(2);
-let students = Student.generateRandomPeople(5, teachers.map(teacher => teacher.id));
-console.log (teachers);
-console.log (students);
+
+
+let teachers = Teacher.generateRandomPeople(10);
+let students = Student.generateRandomPeople(100, teachers.map(teacher => teacher.id));
+
+console.log(teachers);
+console.log(students);
 
 console.log(teachers[0].getMyStudents(students));
 console.log(students[0].getTeacher(teachers));
+
+// Make sure these are at the top of your file!
+// const fs = require("fs");
+// const path = require("path");
+
+// fs.writeFileSync(path.join(__dirname, "Teachers.json"), JSON.stringify(teachers));
+// fs.writeFileSync(path.join(__dirname, "Students.json"), JSON.stringify(students));
