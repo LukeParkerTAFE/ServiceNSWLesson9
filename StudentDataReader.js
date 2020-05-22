668bd656c1255d4078df22d02e64dcfd9c65575a
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-22 10:21:56 +1000

    Went through exercises and updated student data reader

--- StudentDataReader.js
+++ StudentDataReader.js
@@ -1,12 +1,12 @@
 const Student = require("./Student");
 const fs = require("fs");
 
-class StudentDataReader {
+module.exports = class StudentDataReader {
     constructor(fileName) {
         this.fileName = fileName;
     }
 
-    get fileArray() {
+    getArrayFromFile() {
         return JSON.parse(fs.readFileSync(this.fileName).toString()).map(studentRaw => new Student (
             studentRaw.firstName,
             studentRaw.lastName,
@@ -17,31 +17,29 @@ class StudentDataReader {
         ));
     }
 
-    set fileArray(value) {
+    saveArrayToFile(value) {
         fs.writeFileSync(this.fileName, JSON.stringify(value));
     }
 
     getStudent(id) {
-        return this.fileArray.find(s => s.id == id);
+        return this.getArrayFromFile().find(s => s.id == id);
     }
 
     updateStudent(student) {
-        this.fileArray = this.fileArray.map(s => {
+        this.saveArrayToFile(this.getArrayFromFile().map(s => {
             if (s.id == student.id) {
                 return student;
             } else {
                 return s;
             }
-        });
+        }));
     }
 
     deleteStudent(id) {
-        this.fileArray = this.fileArray.filter(s => s.id != id);
+        this.saveArrayToFile(this.getArrayFromFile().filter(s => s.id != id));
     }
 
     addStudent(student) {
-        this.fileArray = this.fileArray.concat([student]);
+        this.saveArrayToFile(this.getArrayFromFile().concat([student]));
     }
 }
-
-module.exports = StudentDataReader;
